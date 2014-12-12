7012084b07656ba34d4c9b501884a97f22a928f9
Mike Marcacci <mike.marcacci@gmail.com>
2014-12-12 14:58:04 -0800

    Another restructure

--- server.js
+++ server.js
@@ -3,8 +3,18 @@
 var app = require('express')();
 var config = require('./config.json');
 
-// Register App
-app.use('/v1/', require('./api/index.js')(config));
+// TODO: create rethinkdb if it doesn't exist
 
-// Start Listening
+var core = require('./lib/core/index.js')(config);
+var api = require('./lib/api/index.js')(core);
+
+// add to the server
+app.use('/v1/', api);
+
+// error handling
+router.use(function(err, req, res, next){
+	console.error(err);
+});
+
+// start listening
 app.listen(config.port);
