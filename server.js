dab14ee0f3c67370d2cf2ae04e43c02d5149a868
Mike Marcacci <mike.marcacci@gmail.com>
2015-01-20 19:23:35 -0800

    Add note for load balancing

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ else {
 	var config = require(process.env.CONFIG || './config.json');
 	var jennings = require('./lib/core/index.js')(config.core);
 
+	// handle connection errors
+	jennings.$arrdb.on('error', function(err){
+
+		// NOTE: this is where one would fire off an alert, kill this process, or
+		// remove this node from a load balancer. By default, we'll just use continue
+		// to use our local cache until we're able to reconnect.
+
+		jennings.$arrdb.once('reconnect', function(){
+
+			// NOTE: this is where one would re-join a load balancer, etc.
+
+		});
+
+	});
+
 	// add to the server
 	app.use(config.prefix, jennings.router);
 
